bb034ec51c402e75040e1351a954d07cb51926aa
0KSY <uryhon7@gmail.com>
2025-09-06 15:39:31 +0900

    comment 기본 동작 구현

---
+++ bulletin-board/src/main/java/com/solo/bulletin_board/comment/dto/CommentDto.java
@@ -0,0 +1,47 @@
+package com.solo.bulletin_board.comment.dto;
+
+import com.solo.bulletin_board.member.dto.MemberDto;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+import java.time.LocalDateTime;
+
+public class CommentDto {
+
+    @Getter
+    @Setter
+    public static class Post{
+        @NotBlank
+        private String content;
+        @Positive
+        private long memberId;
+        @Positive
+        private long postingId;
+
+        private long parentCommentId;
+    }
+
+    @Getter
+    @Setter
+    public static class Patch{
+        private long commentId;
+        private String content;
+    }
+
+    @Getter
+    @Setter
+    @Builder
+    public static class Response{
+
+        private long commentId;
+        private long postingId;
+        private String content;
+        private LocalDateTime createdAt;
+        private LocalDateTime modifiedAt;
+        private MemberDto.MemberResponse memberResponse;
+
+    }
+}
